8ae5e255d25ce48bcba8e939c9630883d59a4981
Alae Belaizi <albelaiz@c2r1p9.1337.ma>
2025-11-17 15:02:47 +0100

    start

---
+++ Makefile
@@ -0,0 +1,32 @@
+NAME = cub3D
+
+CC = cc
+
+CFLAGS = -Wall -Wextra -Werror
+
+LIBFT = ./ft_libft/libft.a
+
+SRC = 
+
+
+OBJ = $(SRC=.c=.o)
+
+all: $(NAME)
+
+$(LIBFT):
+	@$(MAKE) -C ./ft_libft
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ)
+	@$(MAKE) -C ./ft_libft clean
+
+fclean: clean
+	rm -f $(NAME)
+	@$(MAKE) -C ./ft_libft fclean
+
+re: fclean all
+
+.PHONY :clean
---
+++ cub3d.h
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albelaiz <albelaiz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 13:03:12 by albelaiz          #+#    #+#             */
+/*   Updated: 2025/11/17 15:00:22 by albelaiz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "get_next_line/get_next_line.h"
+# include "./libft/libft.h"
+
+typedef struct s_game
+{
+    //Parsed 
+    
+    // Resolution (R)
+    int     win_width;
+    int     win_height;
+
+    // Texture Paths (NO, SO, WE, EA, S)
+    char    *path_no;
+    char    *path_so;
+    char    *path_we;
+    char    *path_ea;
+    char    *path_sprite;
+
+    // Colors (F, C)
+    int     color_floor;
+    int     color_ceiling;
+
+    // Map Grid
+    char    **map;
+    int     map_width;
+    int     map_height;
+
+    // Player Start Info
+    double  player_start_x;
+    double  player_start_y;
+    char    player_start_dir; // 'N', 'S', 'E', or 'W'
+
+    /* --- MiniLibX / Game State (for later) --- */
+    
+    void    *mlx_ptr;
+    void    *win_ptr;
+    // ...texture image pointers...
+
+}   t_game;
+
+void init_config(t_game *game);
+#endif
